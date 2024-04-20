a2423d092d8496d4bd156e1459a01522459a2594
RamLearn-1997 <ramchakra97@gmail.com>
2024-04-20 22:38:13 +0530

    Adding all files react js project series

--- weather-app/src/component/weather/index.jsx
+++ weather-app/src/component/weather/index.jsx
@@ -11,7 +11,7 @@ export default function Weather() {
     setLoading(true);
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=8219a465274941c9b752043e579c0e15`
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid={API_KEY}` // use your api key
       );
   
       const data = await response.json(); // Await the JSON parsing
