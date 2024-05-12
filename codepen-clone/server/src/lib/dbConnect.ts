e61d933db21ed00a6a5c59c3f41f3bce433356ad
AdityaNambiar <aditya.nambiar24@gmail.com>
2024-05-12 20:02:44 +0530

    Updated Code structure to handle DB connection and closing + changed handling of Promises from try-catch to then-catch

--- codepen-clone/server/src/lib/dbConnect.ts
+++ codepen-clone/server/src/lib/dbConnect.ts
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
 export const dbConnect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI!, {
-      dbName: "webd codepen clone",
+  const response = await mongoose.connect(process.env.MONGO_URI!, {
+      dbName: "webd_codepen_clone",
+    }).then((resp) => {
+      console.log("Connection established!");
+    }).catch((err) => {
+      console.log("Error connecting to database:", err);
     });
-    console.log("Connection established!");
-  } catch (error) {
-    console.error("Error connecting to database:", error);
-  }
-  mongoose.connection.close();
+    return response;
+};
+
+export const dbClose = async () => {
+  await mongoose.connection.close()
+                      .then((resp) => {console.log("Connection closed!!")})
+                      .catch((err) => {console.log("Failed to close connection : "+ err )});
 };
